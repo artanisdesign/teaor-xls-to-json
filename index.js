225fceb86a2afc9305950c8cbb06b9d365fac2b9
Gergely Cziva <info@artanis.hu>
2023-02-21 17:08:14 +0100

    hello world

---
+++ index.js
@@ -0,0 +1,114 @@
+import * as fs from "fs";
+import XLSX, { readFile, set_fs } from "xlsx";
+set_fs(fs);
+
+const workbook = readFile("teaor08_tartalom_2021_06_01.xlsx");
+const wsnames = workbook.SheetNames;
+
+if (wsnames.length === 0) {
+    throw new Error("No sheets found in workbook");
+}
+const worksheet = workbook.Sheets[wsnames[0]];
+
+const teorData = XLSX.utils.sheet_to_json(worksheet, { header: "A" });
+//generate JSON from the sheet loopin through the rows
+
+let sector = ""; //szakágazat
+let category = ""; //ágazat
+let subcategory = "";
+
+let sectorObject = {
+    code: "",
+    title: "",
+    codeAndTitle: "",
+    description: "",
+    inThisSector: "",
+    inThisSectorMore: "",
+    notInThisSector: "",
+    notInThisSectorMore: "",
+};
+let categoryObject = {
+    code: "",
+    title: "",
+    codeAndTitle: "",
+    description: "",
+    inThisCategory: "",
+    inThisCategoryMore: "",
+    notInThisCategory: "",
+    notInThisCategoryMore: "",
+};
+let subcategoryObject = {
+    code: "",
+    title: "",
+    codeAndTitle: "",
+    description: "",
+    inThisSubcategory: "",
+    inThisSubcategoryMore: "",
+    notInThisSubcategory: "",
+    notInThisSubcategoryMore: "",
+};
+
+const activies = []
+
+for (const item of teorData) {
+    const code = item.A;
+    switch (code.length) {
+        case 1:
+            sector = code;
+            sectorObject = {
+                code: code,
+                title: item.B,
+                codeAndTitle: code + " " + item.B,
+                description: item.C,
+                inThisSector: item.D,
+                inThisSectorMore: item.E,
+                notInThisSector: item.F,
+            };
+            break;
+        case 2:
+            category = code;
+            categoryObject = {
+                code: code,
+                title: item.B,
+                codeAndTitle: code + " " + item.B,
+                description: item.C,
+                inThisCategory: item.D,
+                inThisCategoryMore: item.E,
+                notInThisCategory: item.F,
+            };
+            break;
+        case 4:
+            subcategory = code;
+            subcategoryObject = {
+                code: code,
+                title: item.B,
+                codeAndTitle: code + " " + item.B,
+                description: item.C,
+                inThisSubcategory: item.D,
+                inThisSubcategoryMore: item.E,
+                notInThisSubcategory: item.F,
+            };
+            break;
+        case 5:
+            activies.push({
+                code: code,
+                title: item.B,
+                codeAndTitle: code + " " + item.B,
+                description: item.C,
+                inThisActivity: item.D,
+                inThisActivityMore: item.E,
+                notInThisActivity: item.F,
+                sectorID: sector,
+                categoryID: category,
+                subcategoryID: subcategory,
+                sectorObject: sectorObject,
+                categoryObject: categoryObject,
+                subcategoryObject: subcategoryObject,
+            });
+            break;
+        default:
+            break;
+    }
+}
+
+fs.writeFileSync('teaor.json', JSON.stringify(activies, null, 2));
